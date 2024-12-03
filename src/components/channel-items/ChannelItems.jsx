1a17a073a0cc35438dd881dc42d78c8ff191f2d9
JavohirXolmamatov <xolmamatovjavohir2002@gmail.com>
2024-12-03 21:24:24 +0500

    q

--- src/components/channel-items/ChannelItems.jsx
+++ src/components/channel-items/ChannelItems.jsx
@@ -1,5 +1,5 @@
 import { CheckCircle } from "@mui/icons-material";
-import { CardMedia, Typography, CardContent } from "@mui/material";
+import { Box, CardMedia, Typography, CardContent } from "@mui/material";
 import { Link } from "react-router-dom";
 
 function ChannelItems({ item }) {
